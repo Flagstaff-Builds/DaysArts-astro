7861d9c84ec87f3d56b71ee5b905ba60d587822c
Work <jwhiteford@ebsco.com>
2023-12-11 23:32:25 -0700

    completed rental page

--- src/components/tailwindUI/Marketing/Page-Sections/Hero-Sections/with-image-tiles.vue
+++ src/components/tailwindUI/Marketing/Page-Sections/Hero-Sections/with-image-tiles.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white">
-    <header class="absolute inset-x-0 top-0 z-50">
+    <header class="absolute bg-white inset-x-0 top-0 z-50">
       <Navigation/>
     </header>
   </div>
