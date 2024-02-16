a5e6c2ee3dad89d4d2128a7e1253104de7bbe96f
FlagstaffWeb <jeremy@flagstaffwebsites.ca>
2024-02-15 22:13:51 -0700

    theme color changes

--- src/components/html-structure/navigation.vue
+++ src/components/html-structure/navigation.vue
@@ -2,7 +2,7 @@
   <nav class="mx-auto flex max-w-7xl items-center justify-between px-6 lg:px-8 z-auto" aria-label="Global">
     <!-- {{company}} Branding -->
     <div class="flex lg:flex-1">
-      <a href="/" class="-m-1.5 p-1.5 hover:text-gray-600">
+      <a href="/" class="-m-1.5 p-1.5">
         <span class="sr-only">{{company}}</span>
 
         <svg aria-hidden="true" class="w-28 lg:w-48 h-auto max-w-xs fill-current" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 555.05 208.51">
@@ -23,12 +23,9 @@
 
 
 
-
-
-
     <!-- Dropdown and Navigation Items -->
     <div class="flex lg:hidden items-center justify-end">
-      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5" @click="mobileMenuOpen = true">
         <span class="sr-only">Open main menu</span>
         <Bars3Icon class="h-6 w-6" aria-hidden="true" />
       </button>
@@ -37,12 +34,14 @@
     <div class="hidden lg:flex lg:gap-x-1.5 items-center">
       <Menu v-for="item in navigation" :key="item.name" as="div" class="relative inline-block text-left items-center">
         <Popover v-if="item.dropdown" class="hidden relative lg:block">
-          <PopoverButton class="inline-flex w-full items-center justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900">
+          <PopoverButton class="inline-flex w-full items-center justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-white">
             {{ item.name }}
             <ChevronDownIcon class="h-5 w-5" aria-hidden="true" />
           </PopoverButton>
 
-          <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
+          <transition
+            enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1"
+          >
             <PopoverPanel class="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
               <div class="w-screen max-w-md flex-auto overflow-hidden rounded-xl text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
                 <div class="p-4 bg-white">
@@ -72,7 +71,7 @@
             </PopoverPanel>
           </transition>
         </Popover>
-        <a v-else :href="item.href" class="cursor-pointer inline-flex w-full items-center justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900">{{ item.name }}</a>
+        <a v-else :href="item.href" class="cursor-pointer inline-flex w-full items-center justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold">{{ item.name }}</a>
 
         <transition v-if="item.dropdown" enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
           <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
@@ -93,7 +92,7 @@
 
     <!-- Additional Nav Item -->
     <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-      <a href="/volunteer" class="cursor-pointer text-sm font-semibold leading-6 text-gray-900">Get involved <span aria-hidden="true">&rarr;</span></a>
+      <a href="/volunteer" class="cursor-pointer text-sm font-semibold leading-6">Get involved <span aria-hidden="true">&rarr;</span></a>
     </div>
   </nav>
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+        'charade': {
+					'50': '#f5f7fa',
+					'100': '#ebeef3',
+					'200': '#d2dae5',
+					'300': '#aab8cf',
+					'400': '#7c93b4',
+					'500': '#5b769c',
+					'600': '#485e81',
+					'700': '#3b4c69',
+					'800': '#344258',
+					'900': '#2f394b',
+					'950': '#1f2532',
+				},
+      },
+		},
 	},
 	plugins: [
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/typography'),
 	],
 }
+
+
