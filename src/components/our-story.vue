07299aebbcb19ee3edeb340210d0998a31de0a7f
FlagstaffWeb <jeremy@flagstaffwebsites.ca>
2024-02-16 18:59:04 -0700

    added color skeleton to images

--- src/components/our-story.vue
+++ src/components/our-story.vue
@@ -33,7 +33,7 @@
 
       <!-- Image section -->
       <div class="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
-        <img src="https://palacetheatredaysarts.files.wordpress.com/2018/07/last-day-at-theatre-082.jpg" alt="" class="aspect-[5/3] w-full object-cover xl:rounded-xl" />
+        <img src="https://palacetheatredaysarts.files.wordpress.com/2018/07/last-day-at-theatre-082.jpg" alt="" class="aspect-[5/3] w-full object-cover xl:rounded-xl bg-purple-100" />
       </div>
 
       <!-- Values section -->
@@ -49,57 +49,6 @@
           </div>
         </dl>
       </div>
-
-      <!-- Team section -->
-      <!-- <div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-48 lg:px-8">
-        <div class="mx-auto max-w-2xl lg:mx-0">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Spotlight on Excitement: Upcoming Events at DaysArts</h2>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Step into a world of entertainment with our lineup of upcoming concerts and shows at DaysArts, where each event promises to be a memorable highlight in our vibrant cultural calendar.</p>
-        </div>
-        <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6">
-          <li v-for="person in team" :key="person.name">
-            <a :href="person.eventLink">
-              <img class="mx-auto h-48 w-48 rounded-lg object-cover" :src="person.imageUrl" alt="" />
-              <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">{{ person.name }}</h3>
-              <p class="text-sm leading-6 text-gray-600">{{ person.date }}</p>
-            </a>
-          </li>
-        </ul>
-      </div> -->
-
-      <!-- Blog section -->
-      <!-- <div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
-        <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-          <p class="mt-2 text-lg leading-8 text-gray-600">Vel dolorem qui facilis soluta sint aspernatur totam cumque.</p>
-        </div>
-        <div class="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          <article v-for="post in blogPosts" :key="post.id" class="relative isolate flex flex-col justify-end overflow-hidden rounded-xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-            <img :src="post.imageUrl" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover" />
-            <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-            <div class="absolute inset-0 -z-10 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-
-            <div class="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-              <time :datetime="post.datetime" class="mr-8">{{ post.date }}</time>
-              <div class="-ml-4 flex items-center gap-x-4">
-                <svg viewBox="0 0 2 2" class="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
-                  <circle cx="1" cy="1" r="1" />
-                </svg>
-                <div class="flex gap-x-2.5">
-                  <img :src="post.author.imageUrl" alt="" class="h-6 w-6 flex-none rounded-full bg-white/10" />
-                  {{ post.author.name }}
-                </div>
-              </div>
-            </div>
-            <h3 class="mt-3 text-lg font-semibold leading-6 text-white">
-              <a :href="post.href">
-                <span class="absolute inset-0" />
-                {{ post.title }}
-              </a>
-            </h3>
-          </article>
-        </div>
-      </div> -->
     </main>
 
   </div>
@@ -130,25 +79,7 @@ const values = [
       'The Motion Picture Theater Association of Alberta (MPTAA) is a trade organization representing the interests of exhibitors behind more than 300 movie screens in Alberta.',
   }
 ]
-const team = [
-  {
-    name: 'Scarlett Butler',
-    style: 'Canadian country music',
-    imageUrl:
-      'https://d10j3mvrs1suex.cloudfront.net/s:bzglfiles/u/513318/445066852ecbf7f7a1c99ea563cd8e4ca193e01e/original/troy-joni-shelter-2020-low-res-34.jpg',
-    eventLink: '/event/scarlett-butler',
-    date: 'Jan 1, 2023',
-  },
-  {
-    name: 'Jake Vaadeland',
-    style: 'Bluegrass and 50\'s rockabilly',
-    imageUrl:
-      'https://images.squarespace-cdn.com/content/v1/6466b769c4608b6fe0b762c4/7f4a0867-53a5-4bb6-9099-1781263ae84a/jake-vaadeland.jpeg',
-    eventLink: '/event/scarlett-butler',
-    date: 'Jan 1, 2023',
-  },
-  // More people...
-]
+
 
 
 const mobileMenuOpen = ref(false)
