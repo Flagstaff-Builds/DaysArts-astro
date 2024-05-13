33c7ce1f37f7ca411595c673db82a0b3936d3f75
FlagstaffWeb <jeremy@flagstaffwebsites.ca>
2024-05-13 10:30:05 -0600

    new build error fixes

--- src/content/config.ts
+++ src/content/config.ts
@@ -37,7 +37,7 @@ const eventCollection = defineCollection({
     socialImage: image().optional(),
     concertSponsor: z.array(z.string()).optional(),
     receptionSponsor: z.array(z.string()).optional(),
-    past: z.boolean().optional(),
+    // past: z.boolean().optional(),
   }),
 });
 
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+
+// Visitortracking.com snippet
+interface Tracer {
+  new (config: { websiteId: string; async: boolean; debug: boolean }): any;
+}
+
+declare var Tracer: Tracer;
