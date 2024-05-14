c4906ad0f57cb0e87cf3248e905a70efa7e39d39
FlagstaffWeb <jeremy@flagstaffwebsites.ca>
2024-05-14 15:45:02 -0600

    tracking code update

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,8 @@
 
 
 // Visitortracking.com snippet
-interface Tracer {
-  new (config: { websiteId: string; async: boolean; debug: boolean }): any;
-}
+// interface Tracer {
+//   new (config: { websiteId: string; async: boolean; debug: boolean }): any;
+// }
 
-declare var Tracer: Tracer;
+// declare var Tracer: Tracer;
