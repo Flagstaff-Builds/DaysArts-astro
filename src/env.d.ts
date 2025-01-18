7f442f5a4042e5a4093a756bc67ee0fc28ff60e8
FlagstaffWeb <jeremy@flagstaffwebsites.ca>
2025-01-17 19:19:03 -0700

    netlify build error fixes

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,9 +2,23 @@
 /// <reference types="astro/client" />
 
 
-// Visitortracking.com snippet
-// interface Tracer {
-//   new (config: { websiteId: string; async: boolean; debug: boolean }): any;
-// }
+// Extend AstroScript attributes
+declare namespace astroHTML.JSX {
+  interface ScriptHTMLAttributes {
+    'client:only'?: boolean;
+  }
+}
 
-// declare var Tracer: Tracer;
+// Visitortracking.com snippet
+interface Tracer {
+  new (config: { websiteId: string; async: boolean; debug: boolean }): any;
+}
+
+declare var Tracer: Tracer;
+
+// Extend Window interface
+declare global {
+  interface Window {
+    init_tracer: () => void;
+  }
+}
